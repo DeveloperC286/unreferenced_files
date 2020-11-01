2df67f19be9560574f76b586eeb509b0bd609238
C <DeveloperC@protonmail.com>
2020-11-01 23:02:53 +0000

    refactor: moved to Path/PathBuf instead of String

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 extern crate regex;
 
 use std::collections::HashSet;
-use std::fs::{read_dir, read_to_string};
+use std::fs::{read_dir, read_to_string, ReadDir};
+use std::path::{Path, PathBuf};
+use std::process::exit;
 
 use regex::Regex;
 use structopt::StructOpt;
 
 mod cli;
 
+const ERROR_EXIT_CODE: i32 = 1;
+
 fn main() {
     let arguments = cli::Arguments::from_args();
 
-    let files: HashSet<String> = get_files_in_directory(arguments.from);
-    let referenced_files: HashSet<String> =
-        get_files_referenced_in_directory(&files, arguments.search);
+    let files = get_files_in_directory(get_path(&arguments.from));
+    let referenced_files = get_files_referenced_in_directory(&files, get_path(&arguments.search));
 
     for unreferenced_file in files.difference(&referenced_files) {
-        println!("{}", unreferenced_file);
+        println!("{}", unreferenced_file.display());
     }
 }
 
-fn get_files_referenced_in_directory(files: &HashSet<String>, path: String) -> HashSet<String> {
+fn get_files_referenced_in_directory(files: &HashSet<PathBuf>, path: &Path) -> HashSet<PathBuf> {
     let mut files_referenced = HashSet::new();
 
     for entry in read_dir(path).unwrap() {
@@ -32,17 +35,15 @@ fn get_files_referenced_in_directory(files: &HashSet<String>, path: String) -> H
                     let content = read_to_string(path).unwrap();
 
                     for file in files {
-                        let file_regex = Regex::new(file).unwrap();
+                        let file_regex = Regex::new(&*file.display().to_string()).unwrap();
 
                         if file_regex.is_match(&content) {
-                            files_referenced.insert(file.to_string());
+                            files_referenced.insert(file.clone());
                         }
                     }
                 } else {
-                    files_referenced.extend(get_files_referenced_in_directory(
-                        files,
-                        path.display().to_string(),
-                    ));
+                    files_referenced
+                        .extend(get_files_referenced_in_directory(files, path.as_path()));
                 }
             }
             Err(error) => {
@@ -54,18 +55,32 @@ fn get_files_referenced_in_directory(files: &HashSet<String>, path: String) -> H
     files_referenced
 }
 
-fn get_files_in_directory(path: String) -> HashSet<String> {
+fn get_directory_entries(path: &Path) -> ReadDir {
+    match read_dir(path) {
+        Ok(entries) => entries,
+        Err(error) => {
+            println!("{:?}", error);
+            exit(ERROR_EXIT_CODE);
+        }
+    }
+}
+
+fn get_path(path: &str) -> &Path {
+    Path::new(path)
+}
+
+fn get_files_in_directory(path: &Path) -> HashSet<PathBuf> {
     let mut files = HashSet::new();
 
-    for entry in read_dir(path).unwrap() {
-        match entry {
+    for dir_entry in get_directory_entries(path) {
+        match dir_entry {
             Ok(dir_entry) => {
                 let path = dir_entry.path();
 
                 if path.is_file() {
-                    files.insert(path.display().to_string());
+                    files.insert(path);
                 } else {
-                    files.extend(get_files_in_directory(path.display().to_string()));
+                    files.extend(get_files_in_directory(path.as_path()));
                 }
             }
             Err(error) => {
