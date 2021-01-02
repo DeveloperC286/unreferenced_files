cb819273d79dea21f943266604dfe4083cce3163
C <DeveloperC@protonmail.com>
2021-01-02 22:10:00 +0000

    fix: consistent unreferenced files order

--- src/reporter/mod.rs
+++ src/reporter/mod.rs
@@ -3,7 +3,10 @@ use std::collections::HashSet;
 use crate::model::file_path_variants::FilePathVariants;
 
 pub fn print(unreferenced_files: HashSet<FilePathVariants>) {
-    for unreferenced_file in unreferenced_files {
+    let mut sorted_unreferenced_files: Vec<&FilePathVariants> = unreferenced_files.iter().collect();
+    sorted_unreferenced_files.sort();
+
+    for unreferenced_file in sorted_unreferenced_files {
         println!("{}", unreferenced_file.file_relative_path);
     }
 }
