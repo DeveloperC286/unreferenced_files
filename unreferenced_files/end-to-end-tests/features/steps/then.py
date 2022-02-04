aca6493a94d143c653f7b7409713ed5b0134624b
DeveloperC <DeveloperC@protonmail.com>
2022-02-04 00:35:02 +0000

    test: inlining step into existing and asserting on full error

--- unreferenced_files/end-to-end-tests/features/steps/then.py
+++ unreferenced_files/end-to-end-tests/features/steps/then.py
@@ -2,17 +2,13 @@ from behave import *
 
 from utilities import execute_unreferenced_files
 
-MISSING_SEARCH = "error: The following required arguments were not provided:\n    --search <search>...\n\n"
-MISSING_SEARCH_FOR = "error: The following required arguments were not provided:\n    --search-for <search-for>...\n\n"
-ONLY_SEARCH_MUTUALLY_EXCLUSIVE = "error: The argument '--only-search <only-search>...' cannot be used with one or more of the other specified arguments\n"
-IGNORE_SEARCH_MUTUALLY_EXCLUSIVE = "error: The argument '--ignore-search <ignore-search>...' cannot be used with one or more of the other specified arguments\n"
-ONLY_SEARCH_FOR_MUTUALLY_EXCLUSIVE = "error: The argument '--only-search-for <only-search-for>...' cannot be used with one or more of the other specified arguments\n"
-IGNORE_SEARCH_FOR_MUTUALLY_EXCLUSIVE = "error: The argument '--ignore-search-for <ignore-search-for>...' cannot be used with one or more of the other specified arguments\n"
-
 
 @then('unreferenced files are not found.')
 def then_unreferenced_files_not_found(context):
+    # When
     execute_unreferenced_files(context)
+
+    # Then
     assert context.stdout == ""
     assert context.stderr == ""
     assert int(context.exit_code) == 0
@@ -20,55 +16,97 @@ def then_unreferenced_files_not_found(context):
 
 @then('the unreferenced files are "{unreferenced_files}".')
 def then_unreferenced_files_found(context, unreferenced_files):
+    # When
     execute_unreferenced_files(context)
+
+    # Then
     unreferenced_files = unreferenced_files.strip() \
         .strip('\"').replace("\\n", '\n')
     assert context.stdout == unreferenced_files
-    then_nonzero_status_code(context)
+    assert int(context.exit_code) != 0
 
 
-@then('the status code is nonzero.')
 def then_nonzero_status_code(context):
-    if not hasattr(context, 'exit_code'):
-        execute_unreferenced_files(context)
+    # When
+    execute_unreferenced_files(context)
+
+    # Then
     assert int(context.exit_code) != 0
 
 
 @then('printed is an error message detailing that the argument search is missing.')
 def then_search_argument_missing_error(context):
-    execute_unreferenced_files(context)
-    assert starts_with(context.stderr, MISSING_SEARCH)
+    # Given
+    search_argument_missing_error = "error: The following required arguments were not provided:\n" + \
+        "    --search <search>...\n" + \
+        "\n" + \
+        "USAGE:\n" + \
+        "    unreferenced_files [FLAGS] [OPTIONS] --search <search>... --search-for <search-for>...\n" + \
+        "\n" + \
+        "For more information try --help\n"
+
+    # When/Then
     then_nonzero_status_code(context)
+
+    # Then
+    assert context.stderr == search_argument_missing_error
 
 
 @then('printed is an error message detailing that the argument search for is missing.')
 def then_search_for_argument_missing_error(context):
-    execute_unreferenced_files(context)
-    assert starts_with(context.stderr, MISSING_SEARCH_FOR)
+    # Given
+    search_for_argument_missing_error = "error: The following required arguments were not provided:\n" + \
+        "    --search-for <search-for>...\n" + \
+        "\n" + \
+        "USAGE:\n" + \
+        "    unreferenced_files [FLAGS] [OPTIONS] --search <search>... --search-for <search-for>...\n" + \
+        "\n" + \
+        "For more information try --help\n"
+
+    # When/Then
     then_nonzero_status_code(context)
+
+    # Then
+    assert context.stderr == search_for_argument_missing_error
 
 
 @then('printed is an error message detailing that the arguments ignore and only search are mutually exclusive.')
 def then_only_and_ignore_search_mutually_exclusive(context):
-    execute_unreferenced_files(context)
-    assert starts_with(
-        context.stderr,
-        ONLY_SEARCH_MUTUALLY_EXCLUSIVE) or starts_with(
-        context.stderr,
-        IGNORE_SEARCH_MUTUALLY_EXCLUSIVE)
+    # Given
+    mutually_exclusive_end = "\n" + \
+        "USAGE:\n" + \
+        "    unreferenced_files --search <search>... --search-for <search-for>... <--only-search <only-search>...|--ignore-search <ignore-search>...>\n" + \
+        "\n" + \
+        "For more information try --help\n"
+
+    only_search_mutually_exclusive_error = "error: The argument '--only-search <only-search>...' cannot be used with one or more of the other specified arguments\n" + mutually_exclusive_end
+    ignore_search_mutually_exclusive_error = "error: The argument '--ignore-search <ignore-search>...' cannot be used with one or more of the other specified arguments\n" + mutually_exclusive_end
+
+    # When/Then
     then_nonzero_status_code(context)
+
+    # Then
+    assert context.stderr in [
+        only_search_mutually_exclusive_error,
+        ignore_search_mutually_exclusive_error]
 
 
 @then('printed is an error message detailing that the arguments ignore and only search for are mutually exclusive.')
 def then_only_and_ignore_search_for_mutually_exclusive(context):
-    execute_unreferenced_files(context)
-    assert starts_with(
-        context.stderr,
-        ONLY_SEARCH_FOR_MUTUALLY_EXCLUSIVE) or starts_with(
-        context.stderr,
-        IGNORE_SEARCH_FOR_MUTUALLY_EXCLUSIVE)
+    # Given
+    mutually_exclusive_end = "\n" + \
+        "USAGE:\n" + \
+        "    unreferenced_files --search <search>... --search-for <search-for>... <--only-search-for <only-search-for>...|--ignore-search-for <ignore-search-for>...>\n" + \
+        "\n" + \
+        "For more information try --help\n"
+
+    only_search_for_mutually_exclusive_error = "error: The argument '--only-search-for <only-search-for>...' cannot be used with one or more of the other specified arguments\n" + mutually_exclusive_end
+    ignore_search_for_mutually_exclusive_error = "error: The argument '--ignore-search-for <ignore-search-for>...' cannot be used with one or more of the other specified arguments\n" + mutually_exclusive_end
+
+    # When/Then
     then_nonzero_status_code(context)
 
-
-def starts_with(searching, searching_for):
-    return searching.strip().startswith(searching_for.strip())
+    # Then
+    assert context.stderr in [
+        only_search_for_mutually_exclusive_error,
+        ignore_search_for_mutually_exclusive_error]
