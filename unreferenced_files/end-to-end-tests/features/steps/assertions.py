f130b88eb0e7f759050693ee4191dc98c5852437
DeveloperC <DeveloperC@protonmail.com>
2023-10-03 22:23:58 +0100

    style: breaking up long Python assertions

--- unreferenced_files/end-to-end-tests/features/steps/assertions.py
+++ unreferenced_files/end-to-end-tests/features/steps/assertions.py
@@ -1,28 +1,39 @@
 def assert_command_successful(result):
-    assert result.exit_code == 0, f"Expected a zero exit code to indicate a successful execution.\nExit code = '{result.exit_code}'.\n"
+    assert result.exit_code == 0, "Expected a zero exit code to indicate a successful execution.\n" + \
+        f"Exit code = '{result.exit_code}'.\n"
 
 
 def assert_command_unsuccessful(result):
-    assert result.exit_code != 0, f"Expected a non-zero exit code to indicate a unsuccessful execution\nExit code = '{result.exit_code}'.\n"
+    assert result.exit_code != 0, "Expected a non-zero exit code to indicate a unsuccessful execution\n" + \
+        f"Exit code = '{result.exit_code}'.\n"
 
 
 def assert_no_output(result):
-    assert result.stdout == "", f"Expected standard output to be empty.\nStandard output = {result.stdout.encode()}.\n"
+    assert result.stdout == "", "Expected standard output to be empty.\n" + \
+        f"Standard output = {result.stdout.encode()}.\n"
 
 
 def assert_no_errors(result):
-    assert result.stderr == "", f"Expected standard error to be empty.\nStandard error = {result.stderr.encode()}.\n"
+    assert result.stderr == "", "Expected standard error to be empty.\n" + \
+        f"Standard error = {result.stderr.encode()}.\n"
 
 
 def assert_error_equals(result, error):
-    assert result.stderr == error, f"Expected standard error to equal the error.\nStandard error = {result.stderr.encode()}.\nError          = {error.encode()}.\n"
+    assert result.stderr == error, "Expected standard error to equal the error.\n" + \
+        f"Standard error = {result.stderr.encode()}.\n" + \
+        f"Error          = {error.encode()}.\n"
 
 
 def assert_error_is_one_of(result, errors):
-    assert result.stderr in errors, f"Expected standard error to equal one of these errors.\nStandard error = {result.stderr.encode()}.\nErrors         = {errors}.\n"
+    assert result.stderr in errors, "Expected standard error to equal one of these errors.\n" + \
+        f"Standard error = {result.stderr.encode()}.\n" + \
+        f"Errors         = {errors}.\n"
 
 
 def assert_unreferenced_files(result, unreferenced_files):
     unreferenced_files = unreferenced_files.strip() \
-        .strip('\"').replace("\\n", '\n')
-    assert result.stdout == unreferenced_files, f"The unreferenced files was not what was expected.\nExpected =\n {unreferenced_files}.\nActual   = \n {result.stdout}\n"
+        .strip('\"') \
+        .replace("\\n", '\n')
+    assert result.stdout == unreferenced_files, "The unreferenced files was not what was expected.\n" +\
+        f"Expected =\n {unreferenced_files}.\n" + \
+        f"Actual   = \n {result.stdout}\n"
--- unreferenced_files/end-to-end-tests/features/steps/given.py
+++ unreferenced_files/end-to-end-tests/features/steps/given.py
@@ -15,16 +15,14 @@ def reset_context(context):
 
 
 @given('the repository "{remote_repository}" is cloned and checked out at the commit "{commit_hash}".')
-def clone_remote_repository_and_checkout_commit(
-        context, remote_repository, commit_hash):
+def clone_remote_repository_and_checkout_commit(context, remote_repository, commit_hash):
     reset_context(context)
 
-    remote_repository_md5 = hashlib.md5(remote_repository.encode())
-    context.remote_repository_cache = f"/tmp/{remote_repository_md5.hexdigest()}/{commit_hash}"
+    md5 = hashlib.md5(remote_repository.encode()).hexdigest()
+    context.remote_repository_cache = f"/tmp/{md5}/{commit_hash}"
 
     if not os.path.exists(context.remote_repository_cache):
-        result = execute_command(
-            f"git clone {remote_repository} {context.remote_repository_cache}")
+        result = execute_command(f"git clone {remote_repository} {context.remote_repository_cache}")
         assert_command_successful(result)
 
         os.chdir(context.remote_repository_cache)
